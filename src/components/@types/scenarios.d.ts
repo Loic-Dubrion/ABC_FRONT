e1533713a4bbc5fbf6574076700ec67b7df138bd
Canoral <kaya68@outlook.fr>
2023-09-11 23:27:08 +0200

    build: :hammer: Création l'interface pour l'objet scénarios

--- src/components/@types/scenarios.d.ts
+++ src/components/@types/scenarios.d.ts
@@ -1,1 +1,7 @@
-interface IScenarios {}
+interface IScenarios {
+  id: number | null;
+  name: string | null;
+  user_id: number | null;
+  created_at: string | null;
+  updated_at: string | null;
+}
