a48a9cdb50b27e49819e030792205caeec623ff5
Canoral <kaya68@outlook.fr>
2023-09-15 00:45:35 +0200

    build: :wrench: installation chartjs

--- src/components/Modals/SequenceModal.tsx
+++ src/components/Modals/SequenceModal.tsx
@@ -1,5 +1,5 @@
 // React Hooks
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 // Reducers actions
 import {
@@ -14,17 +14,22 @@ interface SequenceModal {
 
 function SequenceModal({ isOpen }: SequenceModal) {
   const { id } = useParams();
-  const dialogRef = useRef<HTMLDialogElement>(null);
   const dispatch = useAppDispatch();
   const [scenarioData, setScenarioData] = useState({
     name: '',
     sequenceId: id as string,
   });
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(updateSequence(scenarioData));
+    dispatch(toggleUpdateSequenceMenu(isOpen));
+  };
+
   return (
-    <dialog id="sqfqsfqsaz" className="modal" open={isOpen} ref={dialogRef}>
+    <dialog id="sqfqsfqsaz" className="modal" open={isOpen}>
       <div className="modal-box">
-        <form action="post">
+        <form action="post" onSubmit={handleSubmit}>
           <h3 className="font-bold text-lg mb-2">
             Modifier le nom du scénario
           </h3>
@@ -37,28 +42,12 @@ function SequenceModal({ isOpen }: SequenceModal) {
                 name: event.target.value,
                 sequenceId: id as string,
               });
+              localStorage.setItem('sequence_name', event.target.value);
             }}
             placeholder="Entrez le nom de scénario"
             className="input input-bordered w-full max-w-xs"
           />
-          <button
-            className="btn btn-success ml-5 text-white"
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch(updateSequence(scenarioData));
-              dialogRef.current?.close();
-              dispatch(toggleUpdateSequenceMenu(isOpen));
-              setScenarioData({
-                name: '',
-                sequenceId: id as string,
-              });
-              setTimeout(() => {
-                window.location.reload();
-              }, 100);
-            }}
-          >
-            Valider
-          </button>
+          <button className="btn btn-success ml-5 text-white">Valider</button>
         </form>
       </div>
       <form method="dialog" className="modal-backdrop">
