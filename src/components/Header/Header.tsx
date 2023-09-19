e5c76ea3434f755a2ef2fe3f48a058d76ecf214b
Canoral <kaya68@outlook.fr>
2023-09-19 21:11:04 +0200

    refactor: :lipstick: Enlever les bord arrondi du header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -45,7 +45,7 @@ function Header() {
   }, [isOpen, dispatch]);
 
   return (
-    <header className="navbar bg-base-300 rounded-box mb-5 w-full sticky top-0 z-10">
+    <header className="navbar bg-base-300 mb-5 w-full sticky top-0 z-10">
       <div className="flex-1 px-2 lg:flex-none">
         <Link to="/" className="text-lg font-bold">
           <img src={logo} alt="logo" width={50} />
