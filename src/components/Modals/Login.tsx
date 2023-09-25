c00a71a7261102bb633b1243123882f13bb489af
Canoral <kaya68@outlook.fr>
2023-09-25 16:55:30 +0200

    refactor: :recycle: Correction les éléments du form à login

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -16,6 +16,7 @@ function Login() {
       loginModalRef.current.showModal();
     }
   }
+
   function closeModal() {
     if (loginModalRef.current) {
       loginModalRef.current.close();
@@ -38,38 +39,44 @@ function Login() {
           <form method="post" onSubmit={handleSubmit}>
             <div className="mb-6">
               <label
-                htmlFor="text"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="username"
               >
                 Votre identifiant
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="pseudo"
+                  value={formData.username}
+                  autoComplete="username"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setFormData({ ...formData, username: e.target.value })
+                  }
+                  required
+                />
               </label>
-              <input
-                type="text"
-                name="username"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="pseudo"
-                value={formData.username}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setFormData({ ...formData, username: e.target.value })
-                }
-                required
-              />
             </div>
             <div className="mb-6">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="password"
+              >
                 Votre mot de passe
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="mot de passe"
+                  value={formData.password}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                  required
+                />
               </label>
-              <input
-                type="password"
-                name="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="mot de passe"
-                value={formData.password}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
-                required
-              />
             </div>
             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full m-auto block sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Connexion
