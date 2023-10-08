fb281316923795baefd50cc787f5af31e38bae82
Canoral <kaya68@outlook.com>
2023-10-08 11:37:53 +0200

    styles: Changement de la couleur du bouton qui permet de toggle le niveau

--- src/components/CreateSequence/TogglerLevelButton.tsx
+++ src/components/CreateSequence/TogglerLevelButton.tsx
@@ -1,27 +1,29 @@
 // Redux
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 // Reducer actions
-import { togglerCheckbox } from '../../redux/store/reducers/card';
+import { togglerCheckbox } from "../../redux/store/reducers/card";
 
 function TogglerLevelButton() {
   const dispatch = useAppDispatch();
   const isChecked = useAppSelector((state) => state.card.isChecked);
   return (
     <div className="flex justify-center items-center gap-3 mt-3">
-      <p className={`${!isChecked ? 'font-bold text-[#3abff8]' : ''}`}>
+      <p className={`${!isChecked ? "font-bold text-[#1d4ed8]" : ""}`}>
         Novice
       </p>
       <input
         id="toggler"
         name="toggler"
         type="checkbox"
-        className="toggle toggle-error toggle-lg bg-[#3abff8]"
+        className={`toggle toggle-lg ${
+          !isChecked ? "bg-[#1d4ed8]" : "bg-[#be1622]"
+        }  `}
         checked={isChecked}
         onChange={() => {
           dispatch(togglerCheckbox(isChecked));
         }}
       />
-      <p className={`${isChecked ? 'font-bold text-[#f87272]' : ''}`}>Expert</p>
+      <p className={`${isChecked ? "font-bold text-[#be1622]" : ""}`}>Expert</p>
     </div>
   );
 }
